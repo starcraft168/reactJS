c7a6bfc353fca0aee0155019e67f2f4aa0407773
starcraft168 <ericeng168@gmail.com>
2017-03-02 01:09:43 -0800

    reactDOM.render() inside of index.js

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -7,3 +7,23 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+//   );
+// }
+
+// setInterval(tick, 1000);
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
